f1222d9b12a422d128190583b65f56668901de23
hugoalh <32359235+hugoalh@users.noreply.github.com>
2024-04-08 18:04:23 +0800

    Add test

---
+++ mod.test.ts
@@ -0,0 +1,29 @@
+import { assertEquals } from "TEST/assert_equals.ts";
+import { Pressure } from "./mod.ts";
+Deno.test("Conversion 1", { permissions: "none" }, async (t) => {
+	const pressureFromBar = new Pressure(1, "Bar");
+	await t.step("To Object", () => {
+		console.log(pressureFromBar.toObject());
+	});
+	await t.step("To String", () => {
+		assertEquals(pressureFromBar.toString(), "100000 Pa");
+	});
+	await t.step("To Value", () => {
+		assertEquals(pressureFromBar.toValue(), 100000);
+	});
+});
+Deno.test("Conversion 2", { permissions: "none" }, async (t) => {
+	const pressureFromPa = new Pressure(100000);
+	await t.step("To Object", () => {
+		console.log(pressureFromPa.toObject());
+	});
+	await t.step("To String", () => {
+		assertEquals(pressureFromPa.toString("Bar"), "1 bar");
+	});
+	await t.step("To Value", () => {
+		assertEquals(pressureFromPa.toValue("Bar"), 1);
+	});
+});
+Deno.test("Units Meta", { permissions: "none" }, () => {
+	console.log(Pressure.units());
+});
